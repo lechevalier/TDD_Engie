63ff7ba776f7631bb5f7aac65c3a7efd2bb501f4
PW5764 <pw5764@engie.com>
2020-10-30 15:21:12 +0100

    init WordWrapper

--- FooBarQix/foobarqix_py_test.py
+++ FooBarQix/foobarqix_py_test.py
@@ -1,10 +1,8 @@
-import unittest
-
-from foobarqix import FooBarQix
+from FooBarQix.foobarqix import FooBarQix
 
 
-class FooBarQixTest(unittest.TestCase):
-    def setUp(self):
+class TestFooBarQix:
+    def setup_method(self):
         self.foo_bar_qix = FooBarQix({
             3: "Foo",
             5: "Bar",
@@ -12,35 +10,31 @@ class FooBarQixTest(unittest.TestCase):
         })
 
     def test_should_return_number_representation_when_number_is_regular(self):
-        self.assertEqual("1", self.foo_bar_qix.generate(1))
+        assert self.foo_bar_qix.generate(1) == "1"
 
     def test_should_return_foo_when_number_is_divisible_by_three(self):
-        self.assertEqual("Foo", self.foo_bar_qix.generate(9))
+        assert self.foo_bar_qix.generate(9) == "Foo"
 
     def test_should_return_bar_when_number_is_divisible_by_five_and_contains_five(self):
-        self.assertEqual("BarBar", self.foo_bar_qix.generate(5))
+        assert self.foo_bar_qix.generate(5) == "BarBar"
 
     def test_should_return_qix_when_number_is_divisible_by_seven_and_doesnt_contain_7(self):
-        self.assertEqual("Qix", self.foo_bar_qix.generate(14))
+        assert self.foo_bar_qix.generate(14) == "Qix"
 
     def test_should_return_foobar_when_number_is_divisible_by_three_and_five_and_contains_five(self):
-        self.assertEqual("FooBarBar", self.foo_bar_qix.generate(15))
+        assert self.foo_bar_qix.generate(15) == "FooBarBar"
 
     def test_should_return_foo_when_number_contains_three_and_is_not_divisible_by_three(self):
-        self.assertEqual("Foo", self.foo_bar_qix.generate(23))
+        assert self.foo_bar_qix.generate(23) == "Foo"
 
     def test_should_return_foofoofoo_when_number_contains_three_twice_and_is_divisible_by_three(self):
-        self.assertEqual("FooFooFoo", self.foo_bar_qix.generate(33))
+        assert self.foo_bar_qix.generate(33) == "FooFooFoo"
 
     def test_should_return_foo_when_number_contains_five_and_is_not_divisible_by_five(self):
-        self.assertEqual("Bar", self.foo_bar_qix.generate(52))
+        assert self.foo_bar_qix.generate(52) == "Bar"
 
     def test_should_return_qix_when_number_is_not_divisible_by_seven_and_contains_7(self):
-        self.assertEqual("Qix", self.foo_bar_qix.generate(71))
+        assert self.foo_bar_qix.generate(71) == "Qix"
 
     def test_should_return_qix_when_number_is_divisible_by_seven_and_contains_7(self):
-        self.assertEqual("QixQix", self.foo_bar_qix.generate(7))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert self.foo_bar_qix.generate(7) == "QixQix"
--- FooBarQix/foobarqix_test.py
+++ FooBarQix/foobarqix_test.py
@@ -1,8 +1,10 @@
-from foobarqix import FooBarQix
+import unittest
+
+from FooBarQix.foobarqix import FooBarQix
 
 
-class TestFooBarQix:
-    def setup_method(self):
+class FooBarQixTest(unittest.TestCase):
+    def setUp(self):
         self.foo_bar_qix = FooBarQix({
             3: "Foo",
             5: "Bar",
@@ -10,31 +12,35 @@ class TestFooBarQix:
         })
 
     def test_should_return_number_representation_when_number_is_regular(self):
-        assert self.foo_bar_qix.generate(1) == "1"
+        self.assertEqual("1", self.foo_bar_qix.generate(1))
 
     def test_should_return_foo_when_number_is_divisible_by_three(self):
-        assert self.foo_bar_qix.generate(9) == "Foo"
+        self.assertEqual("Foo", self.foo_bar_qix.generate(9))
 
     def test_should_return_bar_when_number_is_divisible_by_five_and_contains_five(self):
-        assert self.foo_bar_qix.generate(5) == "BarBar"
+        self.assertEqual("BarBar", self.foo_bar_qix.generate(5))
 
     def test_should_return_qix_when_number_is_divisible_by_seven_and_doesnt_contain_7(self):
-        assert self.foo_bar_qix.generate(14) == "Qix"
+        self.assertEqual("Qix", self.foo_bar_qix.generate(14))
 
     def test_should_return_foobar_when_number_is_divisible_by_three_and_five_and_contains_five(self):
-        assert self.foo_bar_qix.generate(15) == "FooBarBar"
+        self.assertEqual("FooBarBar", self.foo_bar_qix.generate(15))
 
     def test_should_return_foo_when_number_contains_three_and_is_not_divisible_by_three(self):
-        assert self.foo_bar_qix.generate(23) == "Foo"
+        self.assertEqual("Foo", self.foo_bar_qix.generate(23))
 
     def test_should_return_foofoofoo_when_number_contains_three_twice_and_is_divisible_by_three(self):
-        assert self.foo_bar_qix.generate(33) == "FooFooFoo"
+        self.assertEqual("FooFooFoo", self.foo_bar_qix.generate(33))
 
     def test_should_return_foo_when_number_contains_five_and_is_not_divisible_by_five(self):
-        assert self.foo_bar_qix.generate(52) == "Bar"
+        self.assertEqual("Bar", self.foo_bar_qix.generate(52))
 
     def test_should_return_qix_when_number_is_not_divisible_by_seven_and_contains_7(self):
-        assert self.foo_bar_qix.generate(71) == "Qix"
+        self.assertEqual("Qix", self.foo_bar_qix.generate(71))
 
     def test_should_return_qix_when_number_is_divisible_by_seven_and_contains_7(self):
-        assert self.foo_bar_qix.generate(7) == "QixQix"
+        self.assertEqual("QixQix", self.foo_bar_qix.generate(7))
+
+
+if __name__ == '__main__':
+    unittest.main()
