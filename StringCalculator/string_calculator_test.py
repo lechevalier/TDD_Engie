cbc42dfdf09177b9a2b53d9d483dbcde4a80eafc
Youcef Mahboub <youcef.mahboub@engie.external.com>
2020-11-13 16:00:29 +0100

    any delimiter

--- StringCalculator/string_calculator_test.py
+++ StringCalculator/string_calculator_test.py
@@ -1,12 +1,20 @@
-import pytest
-
-
 class StringCalculator:
     @classmethod
     def add(cls, string_number: str):
-        if string_number:
-            return int(string_number)
-        return 0
+        if not string_number:
+            return 0
+
+        delimiter = ","
+        if string_number.startswith("//"):
+            delimiter = string_number[2]
+            string_number = string_number[4:]
+
+        else:
+            string_number = string_number.replace("\n", ",")
+
+        numbers = string_number.split(delimiter)
+
+        return sum(map(int, numbers))
 
 
 class TestStringCalculator:
@@ -15,3 +23,12 @@ class TestStringCalculator:
 
     def test_should_return_number_when_string_is_a_single_number(self):
         assert StringCalculator.add("1") == 1
+
+    def test_should_return_sum_of_numbers_when_string_is_composed_of_several_number_representation(self):
+        assert StringCalculator.add("1,2,3") == 6
+
+    def test_should_return_sum_of_numbers_when_separated_by_comma_and_new_line(self):
+        assert StringCalculator.add("1\n2,3") == 6
+
+    def test_should_return_sum_of_number_in_string_separated_by_given_delimiter(self):
+        assert StringCalculator.add("//;\n1;2") == 3
