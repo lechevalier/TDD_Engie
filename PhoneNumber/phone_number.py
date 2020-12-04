74bff9dbbbc3229da15a8e3464e148e583b530f6
PW5764 <pw5764@engie.com>
2020-12-04 16:05:07 +0100

    add search for prefixes in phonebook

--- PhoneNumber/phone_number.py
+++ PhoneNumber/phone_number.py
@@ -1,12 +1,10 @@
-PHONE_BOOK = [
-            ("Bob", "91 12 54 26"),
-            ("Alice", "97 625 992"),
-            ("Emergency", "911")
-        ]
-
-
 class PhoneNumber:
-    @classmethod
-    def get_name_from_phone_number(cls, number: str):
-        return "Emergency"
+    def __init__(self, phonebook):
+        self.phonebook = {name: number.replace(' ', '') for name, number in phonebook.items()}
 
+    def get_name_from_phone_number(self, number: str):
+        candidates = []
+        for book_name, book_number in self.phonebook.items():
+            if number.startswith(book_number):
+                candidates.append(book_name)
+        return min(candidates, key=lambda name: len(self.phonebook[name]))
--- PhoneNumber/test_phone_number.py
+++ PhoneNumber/test_phone_number.py
@@ -4,7 +4,18 @@ from PhoneNumber.phone_number import PhoneNumber
 class TestPhoneNumber:
 
     def setup_class(self):
-        self.phone_number = PhoneNumber()
+        phonebook = {
+            "Bob": "91 12 54 26",
+            "Alice": "97 625 992",
+            "Emergency": "911",
+        }
+        self.phone_number = PhoneNumber(phonebook)
 
     def test_dial_911_should_call_emergency(self):
-        self.phone_number.get_name_from_phone_number("911")
+        assert self.phone_number.get_name_from_phone_number("911") == "Emergency"
+
+    def test_dial_97625992_should_call_alice(self):
+        assert self.phone_number.get_name_from_phone_number("97625992") == "Alice"
+
+    def test_dial_91125426_should_call_emergency(self):
+        assert self.phone_number.get_name_from_phone_number("91125426") == "Emergency"
