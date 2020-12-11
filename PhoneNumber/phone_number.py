e5024afbc0c4f8e859380d905493c5c0c48d0925
Youcef Mahboub <youcef.mahboub@engie.external.com>
2020-12-11 15:41:23 +0100

    added file parsing + consistency beginning

--- PhoneNumber/phone_number.py
+++ PhoneNumber/phone_number.py
@@ -1,12 +1,26 @@
-PHONE_BOOK = [
-            ("Bob", "91 12 54 26"),
-            ("Alice", "97 625 992"),
-            ("Emergency", "911")
-        ]
-
-
 class PhoneNumber:
-    @classmethod
-    def get_name_from_phone_number(cls, number: str):
-        return "Emergency"
+    def __init__(self, phonebook):
+        self.phonebook = {name: number.replace(' ', '').replace('-', '') for name, number in phonebook.items()}
 
+    def __eq__(self, other: "PhoneNumber"):
+        return other.phonebook == self.phonebook
+
+    @classmethod
+    def from_file(cls, filename) -> "PhoneNumber":
+        with open(filename) as f:
+            phone_book = {}
+            for i, line in enumerate(f):
+                if i > 0:
+                    name, number = line.rstrip('\n').split(',')
+                    phone_book[name] = number
+        return cls(phone_book)
+
+    def get_name_from_phone_number(self, number: str):
+        candidates = []
+        for book_name, book_number in self.phonebook.items():
+            if number.startswith(book_number):
+                candidates.append(book_name)
+        return min(candidates, key=lambda name: len(self.phonebook[name]))
+
+    def validate_consistency(self) -> bool:
+        return False
--- PhoneNumber/test_phone_number.py
+++ PhoneNumber/test_phone_number.py
@@ -4,7 +4,32 @@ from PhoneNumber.phone_number import PhoneNumber
 class TestPhoneNumber:
 
     def setup_class(self):
-        self.phone_number = PhoneNumber()
+        phonebook = {
+            "Bob": "91 12 54 26",
+            "Alice": "97 625 992",
+            "Emergency": "911",
+        }
+        self.phone_number = PhoneNumber(phonebook)
 
     def test_dial_911_should_call_emergency(self):
-        self.phone_number.get_name_from_phone_number("911")
+        assert self.phone_number.get_name_from_phone_number("911") == "Emergency"
+
+    def test_dial_97625992_should_call_alice(self):
+        assert self.phone_number.get_name_from_phone_number("97625992") == "Alice"
+
+    def test_dial_91125426_should_call_emergency(self):
+        assert self.phone_number.get_name_from_phone_number("91125426") == "Emergency"
+
+    def test_parsing_phone_book_file_returns_a_dictionary(self):
+        filename = "phone_data_mock.txt"
+        phone_mock = PhoneNumber({
+            "Bob": "91125426",
+            "Alice": "97625992",
+            "Emergency": "911",
+            "Bobby": "91125426",
+            "Alicia": "97625992",
+        })
+        assert self.phone_number.from_file(filename) == phone_mock
+
+    def test_validate_phone_book_consistency_returns_False(self):
+        assert not self.phone_number.validate_consistency()
