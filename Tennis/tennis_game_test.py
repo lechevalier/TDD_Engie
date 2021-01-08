084a402be1a81fefcf3a74ab1144f37a204a7d03
PW5764 <pw5764@engie.com>
2021-01-08 16:15:47 +0100

    add win rules for tennis game

--- Tennis/tennis_game_test.py
+++ Tennis/tennis_game_test.py
@@ -1,6 +1,3 @@
-import unittest
-
-
 LIST_SCORES = ["Love", "Fifteen", "Thirty", "Forty"]
 
 
@@ -12,39 +9,90 @@ class TennisGame(object):
         }
 
     def get_score(self):
+        diff = abs(int.__sub__(*self.scores.values()))
+        advantage = max(self.scores, key=self.scores.get)
+        if all(score >= 3 for score in self.scores.values()):
+            if diff == 0:
+                return "Deuce"
+            elif diff == 1:
+                return f"Advantage {advantage}"
+            else:
+                return f"Win for {advantage}"
+        if self.scores[advantage] == 4:
+            return f"Win for {advantage}"
         return " - ".join(LIST_SCORES[score] for score in self.scores.values())
 
-    def won_score(self, player_name):
+    def won_point(self, player_name):
         self.scores[player_name] += 1
 
 
-class TestTennisGame(unittest.TestCase):
+class TestTennisGame:
     def test_get_score_should_return_love_love_when_game_not_started(self):
         game = TennisGame()
-        self.assertEqual("Love - Love", game.get_score())
+        assert game.get_score() == "Love - Love"
 
     def test_get_score_should_return_fifteen_love_when_player1_scored_once(self):
         game = TennisGame()
-        game.won_score("player1")
-        self.assertEqual("Fifteen - Love", game.get_score())
+        game.won_point("player1")
+        assert game.get_score() == "Fifteen - Love"
 
     def test_get_score_should_return_love_fifteen_when_player2_scored_once(self):
         game = TennisGame()
-        game.won_score("player2")
-        self.assertEqual("Love - Fifteen", game.get_score())
+        game.won_point("player2")
+        assert game.get_score() == "Love - Fifteen"
 
     def test_get_score_should_return_thirty_love_when_player1_scored_twice(self):
         game = TennisGame()
-        game.won_score("player1")
-        game.won_score("player1")
-        self.assertEqual("Thirty - Love", game.get_score())
+        game.won_point("player1")
+        game.won_point("player1")
+        assert game.get_score() == "Thirty - Love"
 
-    def test_get_score_should_return_thirty_love_when_player1_scored_twice(self):
+    def test_get_score_should_return_deuce_when_player1_and_player2_scored_three_times_both(self):
         game = TennisGame()
-        game.won_score("player1")
-        game.won_score("player1")
-        self.assertEqual("Thirty - Love", game.get_score())
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player2")
+        assert game.get_score() == "Deuce"
 
+    def test_get_score_should_return_advantage_player1_when_player1_scores_a_point_after_deuce(self):
+        game = TennisGame()
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player1")
+        assert game.get_score() == "Advantage player1"
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_get_score_should_return_win_player1_when_player1_scores_four_points(self):
+        game = TennisGame()
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player1")
+        assert game.get_score() == "Win for player1"
+
+    def test_get_score_should_return_win_player2_when_player2_scores_four_points(self):
+        game = TennisGame()
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player2")
+        assert game.get_score() == "Win for player2"
+
+    def test_get_score_should_return_advantage_player1_when_player1_scores_two_points_after_deuce(self):
+        game = TennisGame()
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player1")
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player2")
+        game.won_point("player1")
+        game.won_point("player1")
+        assert game.get_score() == "Win for player1"
+
