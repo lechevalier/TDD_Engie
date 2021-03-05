c1536b1f89526956fc1e1878cc5658373498e08e
Youcef Mahboub <youcef.mahboub@engie.external.com>
2021-03-05 15:58:42 +0100

    thibault optimization

--- PrimeFactors/test_prime_factors.py
+++ PrimeFactors/test_prime_factors.py
@@ -1,27 +1,36 @@
 import math
 
+import pytest
 
-class PrimeFactors(object):
-    @classmethod
-    def is_prime(cls, number):
-        if number < 2:
-            return False
 
-        if number == 2:
-            return True
-
-        return all(number % i for i in range(2, math.floor(math.sqrt(number)) + 1))
-
+class PrimeFactors:
     @classmethod
     def generate(cls, number):
-        primes = []
-        for i in range(2, math.floor(math.sqrt(number)) + 1):
-            if cls.is_prime(i):
-                while number % i == 0:
-                    primes.append(i)
-                    number = number // i
+        def _test_divisor(divisor):
+            nonlocal number
+            while number % divisor == 0:
+                primes.append(divisor)
+                number //= divisor
 
-        if cls.is_prime(number):
+        primes = []
+        _test_divisor(2)
+        _test_divisor(3)
+        # 5, 7, 11, 13, 17, 19, 23, 25, 29
+
+        i = 5
+        # delta = 2
+
+        steps = (2, 4)
+        j = 0
+        while i <= math.floor(math.sqrt(number)):
+            _test_divisor(i)
+            i += steps[j]
+            # j = not j
+
+            # i += delta
+            # delta = 6 - delta
+
+        if number > 1:
             primes.append(number)
 
         return primes
@@ -51,4 +60,17 @@ class TestPrimeFactors:
 
     def test_generate_prime_factors_of_12(self):
         assert PrimeFactors.generate(12) == [2, 2, 3]
+
+    def test_generate_prime_factors_of_mersenne_prime(self):
+        assert PrimeFactors.generate(2 ** 31 - 1) == [2 ** 31 - 1]
+
+    def test_generate_prime_factors_of_false_mersenne_prime(self):
+        assert PrimeFactors.generate(2 ** 42 - 1) == [3, 3, 7, 7, 43, 127, 337, 5419]
+
+    def test_generate_prime_factors_of_big_false_mersenne_prime(self):
+        assert PrimeFactors.generate(2 ** 59 - 1) == [179951, 3203431780337]
+
+    @pytest.mark.skip
+    def test_generate_prime_factors_of_big_mersenne_prime(self):
+        assert PrimeFactors.generate(2 ** 62 - 1) == [3, 715827883, 2147483647]
 
