449ef39d85aa8605114f3655708cbe5f4b872724
PW5764 <pw5764@engie.com>
2021-04-02 16:07:09 +0200

    Add exception to factories

--- Args/test_args_parser.py
+++ Args/test_args_parser.py
@@ -3,28 +3,28 @@ from typing import Dict, Callable, Any, Optional, List
 import pytest
 
 
-def bool_factory(input: Optional[str]) -> bool:
-    return False if input is None else True
+def bool_factory(argument: Optional[str]) -> bool:
+    if argument is None:
+        return False
+    if argument == '':
+        return True
+    raise ValueError("Boolean factory has no value associated.")
 
 
-def int_factory(input: Optional[str]) -> int:
-    if input is None:
+def int_factory(argument: Optional[str]) -> int:
+    if argument is None:
         return 0
-    if input and not input.isdigit():
+    if argument and not all(c.isdigit() or c == '-' for c in argument):
         raise ValueError("Should be digits.")
-    return int(input)
+    return int(argument)
 
 
-def str_factory(input: Optional[str]) -> str:
-    return "" if input is None else input
+def str_factory(argument: Optional[str]) -> str:
+    return "" if argument is None else argument
 
 
-def str_list_factory(input: Optional[str]) -> List[str]:
-    return [] if input is None else list(map(str_factory, input.split(',')))
-
-
-def int_list_factory(input: Optional[str]) -> List[str]:
-    return [] if input is None else list(map(int_factory, input.split(',')))
+def list_factory(primitive_factory):
+    return lambda argument: [] if argument is None else list(map(primitive_factory, argument.split(',')))
 
 
 class ArgParser(object):
@@ -41,28 +41,27 @@ class ArgParser(object):
                 if c[0].isalpha():
                     flag_pairs.append(c.rstrip())
                 else:
+                    # Attach argument to previous flag
                     flag_pairs[-1] = flag_pairs[-1] + " -" + c.rstrip()
             except IndexError:
                 raise ValueError(f"Value not allowed for a flag: {c}")
         return flag_pairs
 
-        # return self.flag_regex.findall(input)
-
     def load(self, input: str) -> dict:
         # Defaulting
         args = {flag: factory(None) for flag, factory in self.schema.items()}
 
-        flags = self.split(input)
+        flag_pairs = self.split(input)
 
         # Parsing
-        for flag in flags:
-            f, *v = flag.split()
-            v = ''.join(v)
-            if not f.isalpha():
-                raise ValueError(f"Value not allowed for a flag: {f}")
-            if f not in self.schema:
-                raise KeyError(f"Unknown flag: {f}")
-            args[f] = self.schema[f](v)
+        for pair in flag_pairs:
+            flag, *arguments = pair.split()
+            arguments = ''.join(arguments)
+            if not flag.isalpha():
+                raise ValueError(f"Value not allowed for a flag: {flag}")
+            if flag not in self.schema:
+                raise KeyError(f"Unknown flag: {flag}")
+            args[flag] = self.schema[flag](arguments)
 
         return args
 
@@ -83,10 +82,6 @@ class TestArgsSplitter:
     def test_shouldnt_split_flag_with_integer_value(self):
         with pytest.raises(ValueError):
             ArgParser({}).split("-1")
-
-    def test_shouldnt_split_flag_with_boolean_flag_and_negative_number(self):
-        with pytest.raises(ValueError):
-            ArgParser({}).split("-l -1")
 
 
 class TestArgsParser:
@@ -142,11 +137,15 @@ class TestArgsParser:
         ).load("-l -p 10 -d /usr/logs") == {"l": True, "p": 10, "d": "/usr/logs"}
 
     def test_arg_parser_should_read_str_list_flag(self):
-        assert ArgParser({"g": str_list_factory}).load("-g this,is,a,list") == {"g": ["this", "is", "a", "list"]}
+        assert ArgParser({"g": list_factory(str_factory)}).load("-g this,is,a,list") == {"g": ["this", "is", "a", "list"]}
 
     def test_arg_parser_should_read_int_list_flag(self):
-        assert ArgParser({"d": int_list_factory}).load("-d 1,2,3,4") == {"d": [1, 2, 3, 4]}
+        assert ArgParser({"d": list_factory(int_factory)}).load("-d 1,2,3,4") == {"d": [1, 2, 3, 4]}
 
     def test_arg_parser_should_raise_value_error_for_illegal_value_with_integer_list_flag(self):
         with pytest.raises(ValueError):
-            ArgParser({"d": int_list_factory}).load("-d a,2,3,4")
+            ArgParser({"d": list_factory(int_factory)}).load("-d a,2,3,4")
+
+    def test_shouldnt_split_flag_with_boolean_flag_and_negative_number(self):
+        with pytest.raises(ValueError):
+            ArgParser({"l": bool_factory}).load("-l -1")
