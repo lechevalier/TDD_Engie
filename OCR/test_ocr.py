5593e8e8338a83d02707ddc58b8fc7e1a0277497
PW5764 <pw5764@engie.com>
2021-04-10 08:34:22 +0200

    use case 1

--- OCR/test_ocr.py
+++ OCR/test_ocr.py
@@ -1,67 +1,50 @@
-ASCII_PATTERNS_ROWS = [
-    {
-        "": [1, 4],
-        " _": [0, 2, 3, 5, 6, 7, 8, 9],
-    },
-    {
-        "  |": [1, 7],
-        "| |": [0],
-        "|_": [5, 6],
-        "|_|": [4, 8, 9],
-        " _|": [2, 3],
-    },
-    {
-        "  |": [1, 4, 7],
-        " _|": [3, 5, 9],
-        "|_|": [0, 6, 8],
-        "|_": [2],
-    },
-]
+from itertools import zip_longest
+from pathlib import Path
+from typing import Iterator
 
 
 class OCR:
 
-    @classmethod
-    def read(cls, file_path: str) -> int:
-        candidates = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+    def __init__(self, characters: str, ascii_repr: str):
+        ascii_lines = ascii_repr.splitlines() + ['']
+        self.height = len(ascii_lines)
+        self.width, mod = divmod(len(ascii_lines[0]), len(characters))
+        assert not mod
+        self.reprs = dict(zip(self.split_entry(ascii_lines), characters))
 
-        with open(file_path) as f:
-            print("")
-            for i, line in enumerate(f.read().splitlines()):
-                print(line)
-                candidates.intersection_update(ASCII_PATTERNS_ROWS[i][line])
+    def split_entry(self, entry: tuple[str]) -> Iterator[tuple[tuple[str]]]:
+        assert len(entry) == self.height and not entry[-1].rstrip()
+        return zip(*[zip_longest(*entry[:-1], fillvalue=' ')] * self.width)
 
-        assert len(candidates) == 1
-        return candidates.pop()
+    def split_text(self, text: str) -> Iterator[tuple[str]]:
+        lines = text.replace('\n', ' ' * (self.width - 1) + '\n').splitlines() + ['']
+        return zip(*[iter(lines)] * self.height)
+
+    def read_file(self, path: Path) -> list[str]:
+        return [''.join(self.reprs[digit] for digit in self.split_entry(entry))
+                for entry in self.split_text(path.read_text())]
 
 
 class TestOCR:
-    def test_read_file_digit_0_should_return_0(self):
-        assert OCR.read("digits/digit_0.txt") == 0
+    def setup_class(cls):
+        ascii_digits = '''\
+ _     _  _     _  _  _  _  _ 
+| |  | _| _||_||_ |_   ||_||_|
+|_|  ||_  _|  | _||_|  ||_| _|
+'''
+        cls.digit_ocr = OCR('0123456789', ascii_digits)
 
-    def test_read_file_digit_1_should_return_1(self):
-        assert OCR.read("digits/digit_1.txt") == 1
-
-    def test_read_file_digit_2_should_return_2(self):
-        assert OCR.read("digits/digit_2.txt") == 2
-        
-    def test_read_file_digit_3_should_return_3(self):
-        assert OCR.read("digits/digit_3.txt") == 3
-
-    def test_read_file_digit_4_should_return_4(self):
-        assert OCR.read("digits/digit_4.txt") == 4
-
-    def test_read_file_digit_5_should_return_5(self):
-        assert OCR.read("digits/digit_5.txt") == 5
-        
-    def test_read_file_digit_6_should_return_6(self):
-        assert OCR.read("digits/digit_6.txt") == 6
-
-    def test_read_file_digit_7_should_return_7(self):
-        assert OCR.read("digits/digit_7.txt") == 7
-
-    def test_read_file_digit_8_should_return_8(self):
-        assert OCR.read("digits/digit_8.txt") == 8
-
-    def test_read_file_digit_9_should_return_9(self):
-        assert OCR.read("digits/digit_9.txt") == 9
+    def test_use_case_1(self):
+        assert self.digit_ocr.read_file(Path('use_case_1.txt')) == [
+            '000000000',
+            '111111111',
+            '222222222',
+            '333333333',
+            '444444444',
+            '555555555',
+            '666666666',
+            '777777777',
+            '888888888',
+            '999999999',
+            '123456789',
+        ]
